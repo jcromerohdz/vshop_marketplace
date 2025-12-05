95e6171764d0c95560fb54ff823c1c0e105f3bca
ITT <jcromerohdz@gmail.com>
2025-12-05 09:45:27 -0800

    Vshop with core application for displaying products

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn VShop.wsgi
---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Category, Item
+
+
+# Create your views here.
+def index(request):
+    context = {
+        "msg": "Hola Somos VShop by Victoria Romero"
+    }
+
+    return render(request, "core/index.html", context)
+
+def home(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    context = {
+        'items': items,
+        'categories': categories
+    }
+    return render(request, 'core/index.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'core/contact.html', context)
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
+    context = {
+        'item': item,
+        'related_items': related_items
+    }
+
+    return render(request, 'core/item.html', context)
